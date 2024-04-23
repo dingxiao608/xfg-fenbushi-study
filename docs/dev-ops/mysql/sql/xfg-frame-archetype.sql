91c5b3c15bfcc1f6ab5e65ab84b8afd381d71adc
dingxiao <929751708@qq.com>
2024-04-23 21:09:59 +0800

    feat: init project

--- docs/dev-ops/mysql/sql/xfg-frame-archetype.sql
+++ docs/dev-ops/mysql/sql/xfg-frame-archetype.sql
@@ -106,3 +106,58 @@ BEGIN;
 INSERT INTO `employee_salary_adjust` VALUES (1, '10000001', '109089990198888811', 1000.00, 800.00, 200.00, '2023-07-14 16:55:53', '2023-07-14 16:55:53');
 INSERT INTO `employee_salary_adjust` VALUES (2, '10000001', '100908977676001', 100.00, 20.00, 80.00, '2023-07-14 21:57:39', '2023-07-14 21:57:39');
 COMMIT;
+
+
+CREATE TABLE `strategy` (
+                            `id` bigint unsigned NOT NULL AUTO_INCREMENT COMMENT '自增ID',
+                            `strategy_id` bigint NOT NULL COMMENT '抽奖策略ID',
+                            `strategy_desc` varchar(128) DEFAULT NULL,
+                            `create_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+                            `update_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
+                            PRIMARY KEY (`id`),
+                            KEY `idx_strategy_id` (`strategy_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+
+CREATE TABLE `strategy_award` (
+                                  `id` bigint unsigned NOT NULL AUTO_INCREMENT COMMENT '自增ID',
+                                  `strategy_id` bigint NOT NULL COMMENT '抽奖策略ID',
+                                  `award_id` int NOT NULL COMMENT '抽奖奖品ID - 内部流转使用',
+                                  `award_title` varchar(128) NOT NULL COMMENT '抽奖奖品标题',
+                                  `award_subtitle` varchar(128) DEFAULT NULL COMMENT '抽奖奖品副标题',
+                                  `award_count` int NOT NULL DEFAULT '0' COMMENT '奖品库存总量',
+                                  `award_count_surplus` int NOT NULL DEFAULT '0' COMMENT '奖品库存剩余',
+                                  `award_rate` decimal(6,4) NOT NULL COMMENT '奖品中奖概率',
+                                  `rule_models` varchar(256) DEFAULT NULL COMMENT '规则模型，rule配置的模型同步到此表，便于使用',
+                                  `sort` int NOT NULL DEFAULT '0' COMMENT '排序',
+                                  `create_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+                                  `update_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '修改时间',
+                                  PRIMARY KEY (`id`),
+                                  KEY `idx_strategy_id_award_id` (`strategy_id`,`award_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+
+CREATE TABLE `strategy_rule` (
+                                 `id` bigint unsigned NOT NULL AUTO_INCREMENT COMMENT '自增ID',
+                                 `strategy_id` int NOT NULL COMMENT '抽奖策略ID',
+                                 `award_id` int DEFAULT NULL COMMENT '抽奖奖品ID【规则类型为策略，则不需要奖品ID】',
+                                 `rule_type` tinyint(1) NOT NULL DEFAULT '0' COMMENT '抽象规则类型；1-策略规则、2-奖品规则',
+                                 `rule_model` varchar(16) NOT NULL COMMENT '抽奖规则类型【rule_random - 随机值计算、rule_lock - 抽奖几次后解锁、rule_luck_award - 幸运奖(兜底奖品)】',
+                                 `rule_value` varchar(64) NOT NULL COMMENT '抽奖规则比值',
+                                 `rule_desc` varchar(128) NOT NULL COMMENT '抽奖规则描述',
+                                 `create_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+                                 `update_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
+                                 PRIMARY KEY (`id`),
+                                 KEY `idx_strategy_id_award_id` (`strategy_id`,`award_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+CREATE TABLE big_market.award(
+                                 id int(11) unsigned not NULL AUTO_INCREMENT COMMENT '自增ID',
+                                 award_id int(8) NOT NULL COMMENT '抽奖奖品ID – 内部流转使用',
+                                 award_key varchar(32) NOT NULL COMMENT '奖品对接标识 – 每一个都是一个对应的发奖策略',
+                                 award_config varchar(32) NOT NULL COMMENT '奖品配置信息',
+                                 award_desc varchar(128) NOT NULL COMMENT '奖品内容描述',
+                                 create_time datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+                                 update_time datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
+                                 PRIMARY KEY (id)
+)ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
